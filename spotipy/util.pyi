2f4a581f5381d6a00c5405be90138795f6cd948e
Daniel Espino <akathorn@gmail.com>
2021-06-21 20:35:02 +0200

    Format pyi files

--- spotipy/util.pyi
+++ spotipy/util.pyi
@@ -2,4 +2,6 @@ from typing import Any, Optional
 
 CLIENT_CREDS_ENV_VARS: Any
 
-def prompt_for_user_token(username: Optional[Any] = ..., scope: Optional[Any] = ..., client_id: Optional[Any] = ..., client_secret: Optional[Any] = ..., redirect_uri: Optional[Any] = ..., cache_path: Optional[Any] = ..., oauth_manager: Optional[Any] = ..., show_dialog: bool = ...): ...
+
+def prompt_for_user_token(username: Optional[Any] = ..., scope: Optional[Any] = ..., client_id: Optional[Any] = ..., client_secret: Optional[Any]
+                          = ..., redirect_uri: Optional[Any] = ..., cache_path: Optional[Any] = ..., oauth_manager: Optional[Any] = ..., show_dialog: bool = ...): ...
