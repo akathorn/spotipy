d9038514dc0881d12fca7938caff343327780efb
Daniel Espino <akathorn@gmail.com>
2021-07-06 11:21:44 +0200

    Refactor test_endpoint_types.py

--- tests/integration/test_endpoint_types.py
+++ tests/integration/test_endpoint_types.py
@@ -5,67 +5,80 @@ import unittest
 from typing import Any
 
 from spotipy import CLIENT_CREDS_ENV_VARS as CCEV
-from spotipy import (CacheFileHandler, Spotify, SpotifyClientCredentials,
-                     SpotifyException, SpotifyOAuth, SpotifyPKCE, json_types)
+from spotipy import Spotify, SpotifyClientCredentials, SpotifyOAuth
 from tests import helpers
 from tests.typechecker import TypeChecker
 
 
-def _make_spotify(scopes=None, retries=None):
+class _TypeTest(unittest.TestCase):
+    """Provides basic functionality to test that a Spotify JSON response matches the expected type.
 
-    retries = retries or Spotify.max_retries
+    The tests:
+        "SPOTIPY_CLIENT_ID"
+        "SPOTIPY_CLIENT_SECRET"
+    """
+    four_tracks = [
+        "spotify:track:6RtPijgfPKROxEzTHNRiDp",
+        "spotify:track:7IHOIqZUUInxjVkko181PB",
+        "4VrWlk8IQxevMvERoX08iC",
+        "http://open.spotify.com/track/3cySlItpiPiIAzU3NyHCJf",
+    ]
 
-    auth_manager = SpotifyOAuth(
-        scope=scopes,
-        cache_handler=CacheFileHandler()
+    scopes: str = (
+        "playlist-modify-public "
+        "user-library-read "
+        "user-follow-read "
+        "user-library-modify "
+        "user-read-private "
+        "user-top-read "
+        "user-follow-modify "
+        "user-read-recently-played "
+        "ugc-image-upload "
+        "user-read-playback-state"
     )
 
-    spotify = Spotify(
-        auth_manager=auth_manager,
-        retries=retries
-    )
+    def assertType(self, value: Any, function: Any) -> None:
+        typechecker = TypeChecker()
+        result = typechecker.compare_with_signature(value, function)
+        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
 
-    return spotify
+    @staticmethod
+    def _get_username() -> str:
+        assert CCEV["client_username"] in os.environ
+        return os.environ[CCEV["client_username"]]
+
+    @classmethod
+    def _make_spotify(cls) -> Spotify:
+        auth_manager = SpotifyOAuth(scope=cls.scopes)
+        return Spotify(auth_manager=auth_manager)
 
 
-class SpotipyNonUserEndpointTypes(unittest.TestCase):
+class SpotipyNonUserEndpointTypes(_TypeTest):
     """
     These tests require client authentication - provide client credentials
     using the following environment variables
 
     ::
 
-        'SPOTIPY_CLIENT_ID'
-        'SPOTIPY_CLIENT_SECRET'
+        "SPOTIPY_CLIENT_ID"
+        "SPOTIPY_CLIENT_SECRET"
     """
 
-    four_tracks = ["spotify:track:6RtPijgfPKROxEzTHNRiDp",
-                   "spotify:track:7IHOIqZUUInxjVkko181PB",
-                   "4VrWlk8IQxevMvERoX08iC",
-                   "http://open.spotify.com/track/3cySlItpiPiIAzU3NyHCJf"]
-
-    creep_urn = 'spotify:track:6b2oQwSGFkzsMtQruIWm2p'
-    el_scorcho_urn = 'spotify:track:0Svkvt5I79wficMFgaqEQJ'
-    pinkerton_urn = 'spotify:album:04xe676vyiTeYNXw15o9jT'
-    weezer_urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
-    pablo_honey_urn = 'spotify:album:6AZv3m27uyRxi8KyJSfUxL'
-    radiohead_urn = 'spotify:artist:4Z8W4fKeB5YxbusRsdQVPb'
-    heavyweight_urn = 'spotify:show:5c26B28vZMN8PG0Nppmn5G'
-    reply_all_urn = 'spotify:show:7gozmLqbcbr6PScMjc0Zl4'
-    heavyweight_ep1_urn = 'spotify:episode:68kq3bNz6hEuq8NtdfwERG'
-    reply_all_ep1_urn = 'spotify:episode:1KHjbpnmNpFmNTczQmTZlR'
+    creep_urn = "spotify:track:6b2oQwSGFkzsMtQruIWm2p"
+    el_scorcho_urn = "spotify:track:0Svkvt5I79wficMFgaqEQJ"
+    pinkerton_urn = "spotify:album:04xe676vyiTeYNXw15o9jT"
+    weezer_urn = "spotify:artist:3jOstUTkEu2JkjvRdBA5Gu"
+    pablo_honey_urn = "spotify:album:6AZv3m27uyRxi8KyJSfUxL"
+    radiohead_urn = "spotify:artist:4Z8W4fKeB5YxbusRsdQVPb"
+    heavyweight_urn = "spotify:show:5c26B28vZMN8PG0Nppmn5G"
+    reply_all_urn = "spotify:show:7gozmLqbcbr6PScMjc0Zl4"
+    heavyweight_ep1_urn = "spotify:episode:68kq3bNz6hEuq8NtdfwERG"
+    reply_all_ep1_urn = "spotify:episode:1KHjbpnmNpFmNTczQmTZlR"
 
     @classmethod
     def setUpClass(cls):
-        cls.spotify = Spotify(
-            auth_manager=SpotifyClientCredentials()
-        )
-        cls.spotify.trace = False
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        # Create a Spotify instance without user authentication
+        cls.spotify = Spotify(auth_manager=SpotifyClientCredentials())
 
     def test_audio_analysis(self):
         result = self.spotify.audio_analysis(self.four_tracks[0])
@@ -120,11 +133,11 @@ class SpotipyNonUserEndpointTypes(unittest.TestCase):
         self.assertType(results, self.spotify.artist_related_artists)
 
     def test_artist_search(self):
-        results = self.spotify.search(q='weezer', type='artist')
+        results = self.spotify.search(q="weezer", type="artist")
         self.assertType(results, self.spotify.search)
 
     def test_artist_search_with_market(self):
-        results = self.spotify.search(q='weezer', type='artist', market='GB')
+        results = self.spotify.search(q="weezer", type="artist", market="GB")
         self.assertType(results, self.spotify.search)
 
     def test_artist_albums(self):
@@ -132,15 +145,15 @@ class SpotipyNonUserEndpointTypes(unittest.TestCase):
         self.assertType(results, self.spotify.artist_albums)
 
     def test_album_search(self):
-        results = self.spotify.search(q='weezer pinkerton', type='album')
+        results = self.spotify.search(q="weezer pinkerton", type="album")
         self.assertType(results, self.spotify.search)
 
     def test_track_search(self):
-        results = self.spotify.search(q='el scorcho weezer', type='track')
+        results = self.spotify.search(q="el scorcho weezer", type="track")
         self.assertType(results, self.spotify.search)
 
     def test_user(self):
-        user = self.spotify.user(user='plamere')
+        user = self.spotify.user(user="plamere")
         self.assertType(user, self.spotify.user)
 
     def test_show_urn(self):
@@ -171,56 +184,23 @@ class SpotipyNonUserEndpointTypes(unittest.TestCase):
         self.assertType(markets, self.spotify.available_markets)
 
 
-class SpotipyPlaylistApiTest(unittest.TestCase):
+class SpotipyPlaylistApiTest(_TypeTest):
+    other_tracks = [
+        "spotify:track:2wySlB6vMzCbQrRnNGOYKa",
+        "spotify:track:29xKs5BAHlmlX1u4gzQAbJ",
+        "spotify:track:1PB7gRWcvefzu7t3LJLUlf",
+    ]
+
     @classmethod
     def setUpClass(cls):
-        cls.four_tracks = ["spotify:track:6RtPijgfPKROxEzTHNRiDp",
-                           "spotify:track:7IHOIqZUUInxjVkko181PB",
-                           "4VrWlk8IQxevMvERoX08iC",
-                           "http://open.spotify.com/track/3cySlItpiPiIAzU3NyHCJf"]
-        cls.other_tracks = ["spotify:track:2wySlB6vMzCbQrRnNGOYKa",
-                            "spotify:track:29xKs5BAHlmlX1u4gzQAbJ",
-                            "spotify:track:1PB7gRWcvefzu7t3LJLUlf"]
-        assert CCEV['client_username'] in os.environ
-        cls.username: str = os.environ[CCEV['client_username']]
+        cls.username = cls._get_username()
+        cls.spotify = cls._make_spotify()
 
-        # be wary here, episodes sometimes go away forever
-        # which could cause tests that rely on four_episodes
-        # to fail
-
-        cls.four_episodes = [
-            "spotify:episode:7f9e73vfXKRqR6uCggK2Xy",
-            "spotify:episode:4wA1RLFNOWCJ8iprngXmM0",
-            "spotify:episode:32vhLjJjT7m3f9DFCJUCVZ",
-            "spotify:episode:7cRcsGYYRUFo1OF3RgRzdx",
-        ]
-
-        scope = (
-            'playlist-modify-public '
-            'user-library-read '
-            'user-follow-read '
-            'user-library-modify '
-            'user-read-private '
-            'user-top-read '
-            'user-follow-modify '
-            'user-read-recently-played '
-            'ugc-image-upload '
-            'user-read-playback-state'
-        )
-
-        cls.spotify = _make_spotify(scopes=scope)
-        cls.spotify_no_retry = _make_spotify(scopes=scope, retries=0)
-
-        cls.new_playlist_name = 'spotipy-playlist-test'
+        cls.new_playlist_name = "spotipy-playlist-test"
         cls.new_playlist = helpers.get_spotify_playlist(
             cls.spotify, cls.new_playlist_name, cls.username) or \
             cls.spotify.user_playlist_create(cls.username, cls.new_playlist_name)
-        cls.new_playlist_uri = cls.new_playlist['uri']
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        cls.new_playlist_uri = cls.new_playlist["uri"]
 
     def test_user_playlists(self):
         playlists = self.spotify.user_playlists(self.username, limit=5)
@@ -231,34 +211,34 @@ class SpotipyPlaylistApiTest(unittest.TestCase):
         self.assertType(playlists, self.spotify.current_user_playlists)
 
     def test_playlist_is_following(self):
-        playlist_to_follow_id = '4erXB04MxwRAVqcUEpu30O'
+        playlist_to_follow_id = "4erXB04MxwRAVqcUEpu30O"
         follows = self.spotify.playlist_is_following(playlist_to_follow_id, [self.username])
         self.assertType(follows, self.spotify.playlist_is_following)
 
     def test_playlist_replace_items(self):
         # add tracks to playlist
-        snapshot = self.spotify.playlist_add_items(self.new_playlist['id'], self.four_tracks)
+        snapshot = self.spotify.playlist_add_items(self.new_playlist["id"], self.four_tracks)
         self.assertType(snapshot, self.spotify.playlist_add_items)
 
         # replace with 3 other tracks
-        snapshot = self.spotify.playlist_replace_items(self.new_playlist['id'], self.other_tracks)
+        snapshot = self.spotify.playlist_replace_items(self.new_playlist["id"], self.other_tracks)
         self.assertType(snapshot, self.spotify.playlist_replace_items)
 
-        snapshot = self.spotify.playlist_remove_all_occurrences_of_items(self.new_playlist['id'],
+        snapshot = self.spotify.playlist_remove_all_occurrences_of_items(self.new_playlist["id"],
                                                                          self.other_tracks)
         self.assertType(snapshot, self.spotify.playlist_remove_all_occurrences_of_items)
 
     def test_playlist(self):
-        pl = self.spotify.playlist(self.new_playlist['id'])
+        pl = self.spotify.playlist(self.new_playlist["id"])
         self.assertType(pl, self.spotify.playlist)
 
     def test_playlist_add_items(self):
         # add tracks to playlist
-        snapshot = self.spotify.playlist_add_items(self.new_playlist['id'], self.other_tracks)
+        snapshot = self.spotify.playlist_add_items(self.new_playlist["id"], self.other_tracks)
         self.assertType(snapshot, self.spotify.playlist_add_items)
 
         snapshot = self.spotify.playlist_remove_all_occurrences_of_items(
-            self.new_playlist['id'], self.other_tracks)
+            self.new_playlist["id"], self.other_tracks)
         self.assertType(snapshot, self.spotify.playlist_remove_all_occurrences_of_items)
 
     def test_playlist_cover_image(self):
@@ -271,39 +251,20 @@ class SpotipyPlaylistApiTest(unittest.TestCase):
         self.assertType(res, self.spotify.playlist_cover_image)
 
 
-class SpotipyLibraryApiTests(unittest.TestCase):
+class SpotipyLibraryApiTests(_TypeTest):
+    album_ids = [
+        "spotify:album:6kL09DaURb7rAoqqaA51KU",
+        "spotify:album:6RTzC0rDbvagTSJLlY7AKl",
+    ]
+    episode_ids = [
+        "spotify:episode:3OEdPEYB69pfXoBrhvQYeC",
+        "spotify:episode:5LEFdZ9pYh99wSz7Go2D0g",
+    ]
+
     @classmethod
     def setUpClass(cls):
-        cls.four_tracks = ["spotify:track:6RtPijgfPKROxEzTHNRiDp",
-                           "spotify:track:7IHOIqZUUInxjVkko181PB",
-                           "4VrWlk8IQxevMvERoX08iC",
-                           "http://open.spotify.com/track/3cySlItpiPiIAzU3NyHCJf"]
-        cls.album_ids = ["spotify:album:6kL09DaURb7rAoqqaA51KU",
-                         "spotify:album:6RTzC0rDbvagTSJLlY7AKl"]
-        cls.episode_ids = [
-            "spotify:episode:3OEdPEYB69pfXoBrhvQYeC",
-            "spotify:episode:5LEFdZ9pYh99wSz7Go2D0g"
-        ]
-        cls.username = os.getenv(CCEV['client_username'])
-
-        scope = (
-            'playlist-modify-public '
-            'user-library-read '
-            'user-follow-read '
-            'user-library-modify '
-            'user-read-private '
-            'user-top-read '
-            'user-follow-modify '
-            'user-read-recently-played '
-            'ugc-image-upload '
-            'user-read-playback-state'
-        )
-        cls.spotify = _make_spotify(scopes=scope)
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        cls.username = cls._get_username()
+        cls.spotify = cls._make_spotify()
 
     def test_current_user_saved_tracks(self):
         # TODO make this not fail if someone doesnthave saved tracks
@@ -337,30 +298,11 @@ class SpotipyLibraryApiTests(unittest.TestCase):
         self.spotify.current_user_saved_episodes_delete(self.episode_ids)
 
 
-class SpotipyUserApiTests(unittest.TestCase):
+class SpotipyUserApiTests(_TypeTest):
     @classmethod
     def setUpClass(cls):
-        cls.username: str = os.getenv(CCEV['client_username'])
-
-        scope = (
-            'playlist-modify-public '
-            'user-library-read '
-            'user-follow-read '
-            'user-library-modify '
-            'user-read-private '
-            'user-top-read '
-            'user-follow-modify '
-            'user-read-recently-played '
-            'ugc-image-upload '
-            'user-read-playback-state'
-        )
-
-        cls.spotify = _make_spotify(scopes=scope)
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        cls.username = cls._get_username()
+        cls.spotify = cls._make_spotify()
 
     def test_basic_user_profile(self):
         user = self.spotify.user(self.username)
@@ -383,18 +325,13 @@ class SpotipyUserApiTests(unittest.TestCase):
         self.assertType(response, self.spotify.current_user_top_artists)
 
 
-class SpotipyBrowseApiTests(unittest.TestCase):
+class SpotipyBrowseApiTests(_TypeTest):
     @classmethod
     def setUpClass(cls):
-        cls.spotify = _make_spotify()
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        cls.spotify = cls._make_spotify()
 
     def test_category(self):
-        response = self.spotify.category('rock')
+        response = self.spotify.category("rock")
         self.assertType(response, self.spotify.category)
 
     def test_categories(self):
@@ -403,9 +340,9 @@ class SpotipyBrowseApiTests(unittest.TestCase):
 
     def test_category_playlists(self):
         response = self.spotify.categories()
-        category = 'rock'
-        for cat in response['categories']['items']:
-            cat_id = cat['id']
+        category = "rock"
+        for cat in response["categories"]["items"]:
+            cat_id = cat["id"]
             if cat_id == category:
                 response = self.spotify.category_playlists(category_id=cat_id)
                 self.assertType(response, self.spotify.category_playlists)
@@ -419,60 +356,22 @@ class SpotipyBrowseApiTests(unittest.TestCase):
         self.assertType(response, self.spotify.featured_playlists)
 
 
-class SpotipyFollowApiTests(unittest.TestCase):
+class SpotipyFollowApiTests(_TypeTest):
     @classmethod
     def setUpClass(cls):
-        cls.username = os.getenv(CCEV['client_username'])
-
-        scope = (
-            'playlist-modify-public '
-            'user-library-read '
-            'user-follow-read '
-            'user-library-modify '
-            'user-read-private '
-            'user-top-read '
-            'user-follow-modify '
-            'user-read-recently-played '
-            'ugc-image-upload '
-            'user-read-playback-state'
-        )
-
-        cls.spotify = _make_spotify(scopes=scope)
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        cls.username = cls._get_username()
+        cls.spotify = cls._make_spotify()
 
     def test_current_user_follows(self):
         response = self.spotify.current_user_followed_artists()
         self.assertType(response, self.spotify.current_user_followed_artists)
 
 
-class SpotipyPlayerApiTests(unittest.TestCase):
+class SpotipyPlayerApiTests(_TypeTest):
     @classmethod
     def setUpClass(cls):
-        cls.username = os.getenv(CCEV['client_username'])
-
-        scope = (
-            'playlist-modify-public '
-            'user-library-read '
-            'user-follow-read '
-            'user-library-modify '
-            'user-read-private '
-            'user-top-read '
-            'user-follow-modify '
-            'user-read-recently-played '
-            'ugc-image-upload '
-            'user-read-playback-state'
-        )
-
-        cls.spotify = _make_spotify(scopes=scope)
-
-    def assertType(self, value: Any, function: Any) -> None:
-        typechecker = TypeChecker()
-        result = typechecker.compare_with_signature(value, function)
-        self.assertFalse(result.has_errors, msg="Type mismatch\n" + result.pprint())
+        cls.username = cls._get_username()
+        cls.spotify = cls._make_spotify()
 
     def test_devices(self):
         # No devices playing by default
